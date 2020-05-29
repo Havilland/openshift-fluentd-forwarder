0633da8e3ee9dbfec3a484e0b03c4d72d7b95d47
Havilland <alexander.bartilla@cloudwerkstatt.com>
2020-05-29 11:50:50 +0200

    remove old fluent plugins and add splunk enterprise plugin

--- common-install.sh
+++ common-install.sh
@@ -58,8 +58,6 @@ gem install -N --conservative --minimal-deps --no-document \
   fluent-plugin-kubernetes_metadata_filter \
   fluent-plugin-rewrite-tag-filter \
   'fluent-plugin-remote_syslog:<1.0.0' \
-  fluent-plugin-splunk-ex \
-  fluent-plugin-splunkhec \
   fluent-plugin-splunk-enterprise
 
 # set up directores so that group 0 can have access like specified in
